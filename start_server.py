76aff1cfdaa8dff701a4ec8782217d5c8c43496b
Niels Mündler <n.muendler@tum.de>
2019-07-31 20:01:43 +0200

    Fix message

--- start_server.py
+++ start_server.py
@@ -5,4 +5,4 @@ from pyfronius.tests.mock_server import start_server
 
 if __name__ == '__main__':
     start_server('localhost', 33442)
-    input("Press any key to stop\n")
+    input("Press enter key to stop\n")
